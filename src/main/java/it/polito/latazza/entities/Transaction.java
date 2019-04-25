1eba4dafc86db4f68566b5ea595cc0f770257ba0
Simone Magnani <s265171@studenti.polito.it>
2019-04-25 03:06:15 +0200

    added features get report ()

--- src/main/java/it/polito/latazza/entities/Transaction.java
+++ src/main/java/it/polito/latazza/entities/Transaction.java
@@ -1,11 +1,13 @@
 package it.polito.latazza.entities;
 
 import java.util.Date;
+import java.util.Optional;
 
 public interface Transaction {
 
-    public enum Type{ CONSUMPTION_CASH, CONSUMPTION_BALANCE, BOXPURCHASE, RECHARGE;}
+    public enum Type{ CONSUMPTION_CASH, CONSUMPTION_BALANCE, BOX_PURCHASE, RECHARGE}
     public Date getDate();
     public Integer getAmount();
     public Type getType();
+    public Optional<String> getDirectObject();
 }
--- src/main/java/it/polito/latazza/entities/TransactionImpl.java
+++ src/main/java/it/polito/latazza/entities/TransactionImpl.java
@@ -1,17 +1,24 @@
 package it.polito.latazza.entities;
 
 import java.util.Date;
+import java.util.Optional;
 
 public class TransactionImpl implements Transaction {
 
     private Date date;
     private Integer amount;
     private Type type;
+    private String directObject;
 
     public TransactionImpl(Date date, Integer amount, Type type) {
         this.date = date;
         this.amount = amount;
         this.type = type;
+    }
+
+    public TransactionImpl(Date date, Integer amount, Type type, String directObject){
+        this(date, amount, type);
+        this.directObject = directObject;
     }
 
     @Override
@@ -28,5 +35,11 @@ public class TransactionImpl implements Transaction {
     public Type getType() {
         return this.type;
     }
+
+    @Override
+    public Optional<String> getDirectObject() {
+        return Optional.ofNullable(this.directObject);
+    }
+
 
 }
