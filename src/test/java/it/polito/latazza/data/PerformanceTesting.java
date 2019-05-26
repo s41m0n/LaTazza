56b40ae38b2a8ceb6f9b44def633a1fc6ec7eb6a
Enrico Postolov <enrico.postolov@gmail.com>
2019-05-26 18:40:27 +0200

    Added performance test case for NFR4

--- src/test/java/it/polito/latazza/data/PerformanceTesting.java
+++ src/test/java/it/polito/latazza/data/PerformanceTesting.java
@@ -86,5 +86,33 @@ public class PerformanceTesting {
 		assertTrue(avgTime < 500);
 		
 	}
+	
+	/* Test performance on method buyBoxes() */
+	@Test
+	void testNFR4() throws EmployeeException, BeverageException, NotEnoughBalance {
+		DataImpl d = new DataImpl();
+		
+		d.reset();
+		
+		d.createEmployee("First", "User");
+		d.createBeverage("Beverage1", 100, 20);
+		int empId = d.getEmployeesId().get(0);
+		int bevId = d.getBeveragesId().get(0);
+		d.rechargeAccount(empId, 5000);
+		
+		long begin, end;
+		long totalTime = 0;
+		for(int i = 0; i < 100; i++) {
+			begin = System.currentTimeMillis();
+			d.buyBoxes(bevId, 1);
+			end = System.currentTimeMillis();
+			totalTime += end - begin;
+		}
+		double avgTime = totalTime/100.0;
+		assertTrue(avgTime < 500);
+		
+	}
+	
+	
 
 }
