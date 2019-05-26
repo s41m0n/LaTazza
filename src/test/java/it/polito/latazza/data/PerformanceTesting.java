814ed672499516b9208137e926dff333a42a92bd
Enrico Postolov <enrico.postolov@gmail.com>
2019-05-26 18:48:06 +0200

    Added performance test case for NFR5

--- src/test/java/it/polito/latazza/data/PerformanceTesting.java
+++ src/test/java/it/polito/latazza/data/PerformanceTesting.java
@@ -2,9 +2,12 @@ package it.polito.latazza.data;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.sql.Date;
+
 import org.junit.jupiter.api.Test;
 
 import it.polito.latazza.exceptions.BeverageException;
+import it.polito.latazza.exceptions.DateException;
 import it.polito.latazza.exceptions.EmployeeException;
 import it.polito.latazza.exceptions.NotEnoughBalance;
 import it.polito.latazza.exceptions.NotEnoughCapsules;
@@ -113,6 +116,32 @@ public class PerformanceTesting {
 		
 	}
 	
-	
+	/* Test performance on method getEmployeeReport */
+	@Test
+	void testNFR5() throws EmployeeException, BeverageException, NotEnoughBalance, NotEnoughCapsules, DateException {
+		DataImpl d = new DataImpl();
+		
+		d.reset();
+		
+		d.createEmployee("First", "User");
+		d.createBeverage("Beverage1", 100, 20);
+		int empId = d.getEmployeesId().get(0);
+		int bevId = d.getBeveragesId().get(0);
+		d.rechargeAccount(empId, 5000);
+		d.buyBoxes(bevId, 1);
+		d.sellCapsules(empId, bevId, 1, true);
+		
+		long begin, end;
+		long totalTime = 0;
+		for(int i = 0; i < 100; i++) {
+			begin = System.currentTimeMillis();
+			d.getEmployeeReport(empId, new Date(System.currentTimeMillis()-24*60*60*1000), new Date(System.currentTimeMillis()+24*60*60*1000));
+			end = System.currentTimeMillis();
+			totalTime += end - begin;
+		}
+		double avgTime = totalTime/100.0;
+		assertTrue(avgTime < 500);
+		
+	}
 
 }
